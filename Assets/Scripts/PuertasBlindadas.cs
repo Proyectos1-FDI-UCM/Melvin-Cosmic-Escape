1682071ddc791c30314ca53f942cef201fc2a644
Antonio Povedano <antopove@ucm.es>
2021-04-17 19:23:39 +0200

    Contenedores de vida y puertas blindadas

---
+++ Assets/Scripts/PuertasBlindadas.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuertasBlindadas : MonoBehaviour
+{
+    public int poderPuertas;
+    public Sprite PuertaAbiertaB;
+    SpriteRenderer PuertaCerradaB;
+    int poder;
+
+    void Start()
+    {
+        PuertaCerradaB = GetComponent<SpriteRenderer>();
+
+        print(poder);
+
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        print("aaaaa");
+
+        if (collision.collider.gameObject.GetComponent<PosesionController>() != null && Input.GetKey(KeyCode.E))
+        {
+            print("uuuuuu");
+            poder = collision.collider.gameObject.GetComponent<GameManager>().poderHabilidad;
+            
+            print(poder);
+
+            if (poder == poderPuertas)
+            {
+                PuertaCerradaB.sprite = PuertaAbiertaB;
+                GetComponent<Collider2D>().enabled = false;
+            }
+        }
+    }
+}
