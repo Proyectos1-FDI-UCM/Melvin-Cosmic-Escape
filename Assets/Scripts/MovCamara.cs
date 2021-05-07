d672dad4ef31eca2727e784e8b7cb2be41c2c8b0
Javier <javcomas@ucm.es>
2021-05-07 13:13:13 +0200

    MOV CAMARA CORRECTA
    SOLUCION BUG CAMARA

--- Assets/Scripts/MovCamara.cs
+++ Assets/Scripts/MovCamara.cs
@@ -7,29 +7,21 @@ public class MovCamara : MonoBehaviour
     public Camera Camara;
     public GameObject canvas;
     public float VelocityScale;
-    public GameObject[] lugar;
-    int I=0;
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(FindObjectOfType<MelvinController>())
-        {
-            //Camara.transform.Translate( Vector3.MoveTowards(Camara.transform.position, lugar.transform.position, VelocityScale ));
-            Camara.transform.position= lugar[I].transform.position;  
-            Camara.transform.position = new Vector3(Camara.transform.position.x, Camara.transform.position.y, -10);
-            //Camara.transform.position= lugarCam.transform.position;            
-            canvas.transform.position = lugar[I].transform.position;
-            I++;
-        }
-        if (I == 2) I = 0;
-    }
+    public GameObject[] lugar;    
+    int I=0;      
     private void OnTriggerExit2D(Collider2D other)
     {
         if (FindObjectOfType<MelvinController>())
         {
+            //Camara.transform.Translate( Vector3.MoveTowards(Camara.transform.position, lugar.transform.position, VelocityScale ));
+            Camara.transform.position = lugar[I].transform.position;
+            Camara.transform.position = new Vector3(Camara.transform.position.x, Camara.transform.position.y, -10);
+            //Camara.transform.position= lugarCam.transform.position;            
+            canvas.transform.position = lugar[I].transform.position;
             this.GetComponent<Collider2D>().isTrigger = false;
-            
+            I++;
         }
+        if (I == 2) I = 0;
         Invoke("COLLIDER", 4);
     }
     void COLLIDER()
