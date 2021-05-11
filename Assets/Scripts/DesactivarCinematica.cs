8f222dc694240fd2d7068312063278966a0f0190
elisatodd <elitodd@ucm.es>
2021-05-11 13:12:28 +0200

    Añadida la cinemática inicial
    He modificado el canvas y añadido un script para la reproduccion

---
+++ Assets/Scripts/DesactivarCinematica.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesactivarCinematica : MonoBehaviour
+{
+    void Start()
+    {
+        this.enabled = true;
+
+        Invoke("Desactivar", 29f);
+    }
+
+    void Desactivar()
+    {
+        Debug.Log("Desactivar llamado.");
+        Destroy(this.gameObject);
+    }
+}
