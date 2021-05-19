3d2667694cfb79fcb2b9ed4c47a2c6195cb576e1
Javier Villegas <javilleg@ucm.es>
2021-05-19 12:31:46 +0200

    anadida animacion de la caja

--- Assets/Scripts/Caja.cs
+++ Assets/Scripts/Caja.cs
@@ -6,12 +6,17 @@ public class Caja : MonoBehaviour
 {
     public AudioSource sonidoCaja;
     AudioSource clonSonidoCaja;
+    public Animator caja;
 
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (Input.GetKey(KeyCode.E) && collision.gameObject.GetComponentInChildren<EnemigoFuerte>())
         {
-            Destroy(this.gameObject);
+            caja.SetTrigger("Destruye");
+
+            this.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+
+            Destroy(this.gameObject, 0.09f);
             clonSonidoCaja = (AudioSource)AudioSource.Instantiate(sonidoCaja);
             clonSonidoCaja.Play();
             Destroy(clonSonidoCaja, 3f);
