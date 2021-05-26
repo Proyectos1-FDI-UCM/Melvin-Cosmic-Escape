6c4829d93facd4525e7ad0b8d922bc28bbf3f956
elisatodd <elitodd@ucm.es>
2021-05-26 10:39:18 +0200

    Cambios en los scripts para no activar el menu en cinematicas

--- Assets/DesactivarPanelNivel.cs
+++ Assets/DesactivarPanelNivel.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class DesactivarPanelNivel : MonoBehaviour
 {
+    public bool panelActivo = false;
     private void Start()
     {
+        panelActivo = true;
         Invoke("DesactivarPanel", 2f);
     }
 
     void DesactivarPanel()
     {
         this.gameObject.SetActive(false);
+        panelActivo = false;
         GameManager.GetInstance().pausa = false;
     }
 }
