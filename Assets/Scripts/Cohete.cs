ad590ee505d7cbbbd8903315144841537c18e249
AntonioPove <79278677+AntonioPove@users.noreply.github.com>
2021-06-05 22:42:15 +0200

    Actualizacion de los carteles de paso de nivel (ganar y perder)

--- Assets/Scripts/Cohete.cs
+++ Assets/Scripts/Cohete.cs
@@ -1,11 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Cohete : MonoBehaviour
 {
     GameManager gm;
     public GameObject cinematicaFinal;
+    
     private void Start()
     {
          gm = GameManager.GetInstance();
@@ -13,6 +12,7 @@ public class Cohete : MonoBehaviour
 
     private void OnCollisionStay2D(Collision2D collision)
     {
+
         if (Input.GetKey(KeyCode.E))
         {
             gm.GanarPartida();
