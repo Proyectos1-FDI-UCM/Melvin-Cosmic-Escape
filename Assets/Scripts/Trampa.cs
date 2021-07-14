eed7589eec26bc788315ac488049ca468c2e6477
alvarolozrod <alvarloz@ucm.es>
2021-07-14 18:55:12 +0200

    Trampa del juego puesta y bug arreglado

---
+++ Assets/Scripts/Trampa.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampa : MonoBehaviour
+{
+    private void Update()
+    {
+        Invoke("Desactivar", 2);
+        Invoke("Activar", 4);
+    }
+    void Desactivar()
+    {
+        this.gameObject.SetActive(false);
+    }
+    void Activar()
+    {
+        this.gameObject.SetActive(true);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (this.gameObject.activeSelf && collision.gameObject.GetComponent<MelvinController>())
+        {
+            GameManager.GetInstance().Pisarcharco();
+        }
+    }
+}
